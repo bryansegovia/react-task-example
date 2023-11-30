bd9839f0a24ed5b9fa76c6827d6f556dffb0c816
bryansegovia <bsegovia@espol.edu.ec>
2023-11-29 19:46:20 -0500

    First Commit

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import TaskCard from './TaskCard'
+import {TaskContext} from '../context/TaskContext'
+import {useContext} from 'react'
+
+function TaskList() {
+  const {tasks}= useContext(TaskContext)
+
+  if (tasks.length === 0) {
+    return <h1 className='text-white text-4xl font-bold text-center'>No hay tareas yet</h1>;
+  }
+  return (
+    <div className='grid grid-cols-4 gap-2'>
+      {tasks.map((task)=>(
+      <TaskCard task={task} key={task.id}/>))}
+    </div>
+  );
+}
+
+export default TaskList;
---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState, useEffect } from "react";
+import { tasks as data } from "../data/task";
+
+export const TaskContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => { //Esto es para cargar datos en un inicio
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle, taskDescription) {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle,
+        id: tasks.length,
+        description: taskDescription,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TaskContext.Provider>
+  );
+}
